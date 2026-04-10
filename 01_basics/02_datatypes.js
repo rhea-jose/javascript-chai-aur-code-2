7bf6ccebd95d5e4c8e6caec87abde72a4ae62a1c
rhea-jose <rhejose@gmail.com>
2026-04-10 07:11:19 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+//js ka engine browser me chupa hua hota hai, isliye sab browser pe sikhate hai
+//node se hum w/o browser kar sakte hai
+"use strict " //treats all js code as newer version
+// alert(3+2) we are using node.js not browser
+
+//data types
+// number-> 2 power 53 , bigint, string->"" bool
+// null -> standalone value represents empty value ("" isnt null it is just empty string)
+// undefined (let c);
+// symbol (used in react, to find uniqueness)
+// object 
+let age=23;
+console.log(typeof age)
+console.log(typeof null) //o/p's object 
+console.log(typeof undefined) //o/p's undefined
+
