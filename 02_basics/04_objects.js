e6b501bcb4d6497d4c4c8124a4a9adce281585fe
rhea-jose <rhejose@gmail.com>
2026-04-11 15:42:44 +0000

    objects

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,62 @@
+// const tinderUser= new Object()
+// console.log(tinderUser)
+
+// const tinderUser= new Object()
+// tinderUser.age=32
+// tinderUser.name='Sam'
+// tinderUser.isLoggedIn=false
+// console.log(tinderUser)
+
+// regUser.fullname?.nextName if fullname exists then ok otherwise stop? provides safety when data comes from api's
+
+obj1={
+    1:"a",
+    2:"b"
+}
+obj2={
+    4:"a",
+    3:"b"
+}
+obj4={
+    5:"c"
+}
+// const obj3={obj1,obj2}
+// const obj3=Object.assign(obj1,obj2)
+// const obj3=Object.assign(obj1,obj2,obj4)
+// const obj3={...obj1,...obj2}
+// console.log(obj1)
+// console.log(obj3)
+// console.log(Object.keys(obj3)) // returns an array of keys
+// console.log(Object.values(obj3)) // returns an array of 
+// console.log(Object.entries(obj3)) // returns an array of key value pairs within an array
+// console.log(obj3.hasOwnProperty("1"))
+
+// Destructuring of Objects
+// const course={
+//     courseInstructor: "hitesh"
+// }
+
+// console.log(course.courseInstructor) // difficult to write this each time
+// const {courseInstructor}=course
+// console.log(courseInstructor) //easier
+// const {courseInstructor:instructor}=course
+
+// console.log(instructor) //even more easier
+
+
+// Aaj kal apis se json return hote hai,pehle xml complicated return hote the
+
+// {
+//     "name":"JavaS",
+//     "price":"free"
+// } -> like this in json , the keys are also strings
+// https://api.github.com/users/hiteshchoudhary -> example of json
+
+// lot of times api's are also received in the form of arrays like:
+// [
+//     {},
+//     {},
+//     {}
+// ]
+// javaScript Object Notation
+
